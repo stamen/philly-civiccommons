cfe35b26aa8b3bdf05d2b9de3a44634246c11ac8
Sean Connelley <connelley@stamen.com>
2015-04-21 16:12:39 -0400

    Table sorting

--- js/data-table.js
+++ js/data-table.js
@@ -24,7 +24,35 @@
     // fill in header
     thead.selectAll("th")
       .data(fields)
-      .enter().append("th").text(function(d){return d.label});
+      .enter().append("th")
+        .attr('class', 'sort-cell')
+        .attr('data-key', function(d){
+          return d.key;
+        })
+        .text(function(d){return d.label})
+        .on('click', function(){
+          d3.event.preventDefault ? d3.event.preventDefault() : d3.event.returnValue = false;
+          var me = d3.select(this);
+          var dir = me.attr('data-direction');
+          var key = me.attr('data-key');
+          if (dir === 'descending') {
+            sortColumns(me, 'ascending', key);
+          } else {
+            sortColumns(me, 'descending', key);
+          }
+        })
+          .append('span')
+          .attr('class', 'sort-arrow');
+
+    function sortColumns(elm, dir, col) {
+      thead.selectAll("th").classed('active', false);
+      elm.attr('data-direction', dir);
+      elm.classed('active', true);
+      tbody.selectAll('tr').sort(function(a,b){
+        return d3[dir](a[col], b[col]);
+      });
+    }
+
 
     // pull out fields
     var parsed = [];
@@ -40,8 +68,10 @@
       parsed.push(o);
     });
 
+
+
     // create table rows
-    var tr = d3.select("tbody").selectAll("tr")
+    var tr = tbody.selectAll("tr")
       .data(parsed).enter().append("tr");
 
     // fill in row cells
@@ -50,6 +80,7 @@
       .enter().append("td")
       .text(function(d) {return d});
 
+    sortColumns(thead.select('[data-key="name"]'), 'ascending', 'name');
 
     // Filter tool
     var categories = [];
@@ -137,7 +168,15 @@
 
     return {
       resize: function(size) {
+
         var parentHeight = size.y - 40;
+        filterWrapper.style('height','auto');
+        filterParent.style('height', 'auto');
+
+        //console.log(filterParent.node().offsetHeight, parentHeight)
+        //if (filterParent.node().offsetHeight < parentHeight) return;
+
+
         filterParent.style('height', parentHeight + 'px');
         var titleHeight = parentHeight - (filterTitle.node().offsetHeight + 20);
         filterWrapper.style('height', titleHeight + 'px');
